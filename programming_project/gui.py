4350ca778e220d87db29bd3759ea42900f7aac45
sandy-2604 <133866939+sandy-2604@users.noreply.github.com>
2023-05-31 00:01:12 +0530

    Update gui.py

--- programming_project/gui.py
+++ programming_project/gui.py
@@ -5,4 +5,18 @@ window.title("Movie recommendation system")
 window.geometry("600x500")
 window['background'] = 'pink'
 
+upper_frame = LabelFrame(window, text="Enter Movie Name", bg='pink')
+upper_frame.pack(pady=25)
+
+input_movie_name = StringVar()
+
+movie_input = Entry( upper_frame, font=("Helvetica",30), textvariable=input_movie_name )
+movie_input.grid(row=0, column=0, padx=10, pady=10)
+
+button = Button(upper_frame, text="Recommend", command=search)
+button.grid(row=0, column=1, padx=10)
+
+lower_frame = LabelFrame(window,height=300,width=800)
+lower_frame.pack(pady=25)
+
 window.mainloop()
