d46c70130c3ce68cca7b8f5acbc6acc8af5c7ec2
Aman-Rahaman <133481825+Aman-Rahaman@users.noreply.github.com>
2023-06-01 21:04:25 +0530

    Update main.py

--- programming_project/main.py
+++ programming_project/main.py
@@ -6,10 +6,10 @@ from nltk.stem.porter import PorterStemmer
 from sklearn.metrics.pairwise import cosine_similarity
 from tkinter import *
 
-dataFrames_movies = pd.read_csv("tmdb_5000_movies.csv")
-dataFrames_credits = pd.read_csv("tmdb_5000_credits.csv")
+dataFrames_movies1 = pd.read_csv("movie_dataset_1.csv")
+dataFrames_movies2 = pd.read_csv("movie_dataset_2.csv")
 
-dataFrames_movies = dataFrames_movies.merge(dataFrames_credits,on="title")  # combining movies and credits on the basis of the movie title name
+dataFrames_movies = dataFrames_movies1.merge(dataFrames_movies2,on="title")  # combining movies and credits on the basis of the movie title name
 
 
 dataFrames_movies = dataFrames_movies[['movie_id','title','overview','genres','keywords','cast','crew']] # selecting particular attributes from the dataset which are usefull for making tags
@@ -103,25 +103,56 @@ array_of_similarity_values_for_all_movies = cosine_similarity(vectors)
 numpy.save("array_of_similarity_values_for_all_movies.npy",array_of_similarity_values_for_all_movies)
 final_movie_data.to_pickle('final_movie_data.pkl')
 
+
 # def recommend(x):
 #     index = final_movie_data[ final_movie_data['title'] == x ].index[0]
 #     similarity_values_for_one_movie = array_of_similarity_values_for_all_movies [ index ]
 #     list_of_similar_movies = sorted( list( enumerate( similarity_values_for_one_movie ) ), reverse=True, key=lambda x:x[1] )[1:6]
-#     for i in list_of_similar_movies:
+#     return list_of_similar_movies
+#     # for i in list_of_similar_movies:
+#     #     print( final_movie_data.iloc[ i[0],1 ].title() ) # here title is a string function
+#
+# #*****************************************************************************
+#
+# window = Tk()
+# window.title("Movie recommendation system")
+# window.geometry("600x500")
+#
+# def search():
+#     list_of_recommended_movies = recommend( input_movie_name.get() )
+#     for i in list_of_recommended_movies:
 #         print( final_movie_data.iloc[ i[0],1 ].title() ) # here title is a string function
-
-
-# while True:
-#     str = input("Enter a movie name : ").lower().strip()
-#     recommend(str)
-#     print("*"*30)
-#     print("Do you want to use it again ? ")
-#     ans = input("Enter 'no' to exit or enter 'yes' to use it again : ").strip().lower()
-#     if ans == 'no':
-#         break
-
-
-
-# NEED to check if the input movie name is present or not!!!!!
-# try to not use ast
-# .index[0] ??? in recommend()
+#
+# upper_frame = LabelFrame(window, text="Enter Movie Name")
+# upper_frame.pack(pady=25)
+#
+# input_movie_name = StringVar()
+#
+# movie_input = Entry( upper_frame, font=("Helvetica",30), textvariable=input_movie_name )
+# movie_input.grid(row=0, column=0, padx=10, pady=10)
+#
+# button = Button(upper_frame, text="Recommend", command=search)
+# button.grid(row=0, column=1, padx=10)
+#
+# text_box = Text(window, height=20, width=65, wrap=WORD)
+# text_box.pack(pady=10)
+#
+# window.mainloop()
+#
+# #********************************************************************
+#
+#
+# # while True:
+# #     str = input("Enter a movie name : ").lower().strip()
+# #     recommend(str)
+# #     print("*"*30)
+# #     print("Do you want to use it again ? ")
+# #     ans = input("Enter 'no' to exit or enter 'yes' to use it again : ").strip().lower()
+# #     if ans == 'no':
+# #         break
+#
+#
+#
+# # NEED to check if the input movie name is present or not!!!!!
+# # try to not use ast
+# # .index[0] ??? in recommend()
