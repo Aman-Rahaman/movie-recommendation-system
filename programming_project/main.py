43e4638433ddeac69b2e0f1b51a366bb7cb985b2
Aman-Rahaman <133481825+Aman-Rahaman@users.noreply.github.com>
2023-06-04 03:41:01 +0530

    Update main.py

--- programming_project/main.py
+++ programming_project/main.py
@@ -5,30 +5,30 @@ from sklearn.feature_extraction.text import CountVectorizer
 from nltk.stem.porter import PorterStemmer
 from sklearn.metrics.pairwise import cosine_similarity
 
-dataFrames_movies1 = pd.read_csv("data1.csv")
+dataFrames1 = pd.read_csv("data1.csv")
 print("open first csv file")
 # This dataset1.csv file contains the TMDB_id, actors names and the other people in the unit.
 # TMDB is similar to IMDB. TMDB = 'The Movie Database'
 
-dataFrames_movies2 = pd.read_csv("data2.csv")
+dataFrames2 = pd.read_csv("data2.csv")
 print("open second csv file")
 # This dataset2.csv file contains a lot of data with respect to each movie.
 # Most of the attributes in this are not in much use of use.
 # Therefore we will only take some selective attributes.
 
-dataFrames_movies = dataFrames_movies1.merge(dataFrames_movies2, on="movie_name")
+dataFrames_films = dataFrames1.merge(dataFrames2, on="movie_name")
 print("merging the frames")
 # combining movies and credits on the basis of the movie movie_name name.
 # now attributes from both the files are in one dataframe.
 
 
-dataFrames_movies = dataFrames_movies[['TMDB_id', 'movie_name', 'summary', 'genres', 'labels', 'actors', 'unit']]
+dataFrames_films = dataFrames_films[['TMDB_id', 'movie_name', 'summary', 'genres', 'labels', 'actors', 'unit']]
 print("selecting attributes")
 # selecting particular attributes from the dataset which are usefull for making tags.
 # selecting only those attributes which are helpful in making the tag.
 
 
-dataFrames_movies.dropna(inplace=True)
+dataFrames_films.dropna(inplace=True)
 # removing the rows which contain null values in any of their attributes
 # this is because these attributes are important to make the tags.
 # if for a movie we have null values in any of the attributes, the the system will show wrong result for that movie.
@@ -86,19 +86,19 @@ def fetch_unit(x):
 
 
 print("movie_name to lower case")
-dataFrames_movies['movie_name'] = dataFrames_movies['movie_name'].apply(lower_case)
-
-print("running fetch generes")
-dataFrames_movies['genres'] = dataFrames_movies['genres'].apply(fetch_genres)
-
-print("running fetch labels")
-dataFrames_movies['labels'] = dataFrames_movies['labels'].apply(fetch_labels)
+dataFrames_films['movie_name'] = dataFrames_films['movie_name'].apply(lower_case)
 
 print("running fetch actor")
-dataFrames_movies["actors"] = dataFrames_movies["actors"].apply(fetch_actors)
+dataFrames_films["actors"] = dataFrames_films["actors"].apply(fetch_actors)
+
+print("running fetch labels")
+dataFrames_films['labels'] = dataFrames_films['labels'].apply(fetch_labels)
+
+print("running fetch generes")
+dataFrames_films['genres'] = dataFrames_films['genres'].apply(fetch_genres)
 
 print("running fetch unit")
-dataFrames_movies["unit"] = dataFrames_movies["unit"].apply(fetch_unit)
+dataFrames_films["unit"] = dataFrames_films["unit"].apply(fetch_unit)
 
 
 # function to split the summary into a list
@@ -107,7 +107,7 @@ def splitting_of_string(s):
 
 
 # splitting of summary from string to list
-dataFrames_movies["summary"] = dataFrames_movies["summary"].apply(splitting_of_string)
+dataFrames_films["summary"] = dataFrames_films["summary"].apply(splitting_of_string)
 
 
 # function to remove the back spaces.
@@ -119,26 +119,26 @@ def remove_spaces(x):
 
 
 # removing space from each of the generes so that the generes appear as one
-dataFrames_movies['genres'] = dataFrames_movies['genres'].apply(remove_spaces)
+dataFrames_films['genres'] = dataFrames_films['genres'].apply(remove_spaces)
 print("removing whitespaces from genere")
 
 # removing spaces from labels so that the labels appear as one if their is a space in between
-dataFrames_movies['labels'] = dataFrames_movies['labels'].apply(remove_spaces)
+dataFrames_films['labels'] = dataFrames_films['labels'].apply(remove_spaces)
 print("removing whitespaces from labels")
 
 # removing spaces from actor names otherwise actor names won't come as one name if their is any space
-dataFrames_movies['actors'] = dataFrames_movies['actors'].apply(remove_spaces)
+dataFrames_films['actors'] = dataFrames_films['actors'].apply(remove_spaces)
 print("removing whitespaces from actors")
 
 # removing space from each of the unit person's name because of the same logic
-dataFrames_movies['unit'] = dataFrames_movies['unit'].apply(remove_spaces)
+dataFrames_films['unit'] = dataFrames_films['unit'].apply(remove_spaces)
 print("removing whitespaces from unit")
 
 
-dataFrames_movies['tags'] = dataFrames_movies['genres'] + dataFrames_movies['labels'] +\
-                            dataFrames_movies["actors"] + dataFrames_movies['unit'] + dataFrames_movies["summary"]
+dataFrames_films['tags'] = dataFrames_films['genres'] + dataFrames_films['labels'] +\
+                            dataFrames_films["actors"] + dataFrames_films['unit'] + dataFrames_films["summary"]
 
-final_movie_data = dataFrames_movies[['TMDB_id', 'movie_name', 'tags']]
+final_movie_data = dataFrames_films[['TMDB_id', 'movie_name', 'tags']]
 
 
 # function to join a list to string.
@@ -153,7 +153,7 @@ print("converting tags from a list to a string")
 final_movie_data.loc[:, 'tags'] = final_movie_data["tags"].apply(list_to_string)
 
 
-# funtion for stemming
+# function for stemming
 def stemming(x):
     stemmed_words = []
     for i in x.split():
@@ -161,21 +161,24 @@ def stemming(x):
     return " ".join(stemmed_words)
 
 
+print("stemming...")
 final_movie_data.loc[:, 'tags'] = final_movie_data['tags'].apply(stemming)
 
+print("vectorization...")
 vectors = CountVectorizer(stop_words="english").fit_transform(final_movie_data['tags']).toarray()
 
-
+print("finding cosine similarity values...")
 array_of_similarity_values_for_all_movies = cosine_similarity(vectors)
+
 
 # saving the cosine similarity values. this is in the form of array.
 # we will use it in the gui.
-numpy.save("array_of_similarity_values_for_all_movies.npy", array_of_similarity_values_for_all_movies)
 print("saving or loading the similarity values")
+numpy.save("array_of_similarity_values_for_all_movies.npy", array_of_similarity_values_for_all_movies)
 
 # saving the final dataframe.
 # we will use it in the gui.
-final_movie_data.to_pickle('final_movie_data.pkl')
 print("saving or loading the final dataframe")
+final_movie_data.to_pickle('final_movie_data.pkl')
 
 print("Done...")
